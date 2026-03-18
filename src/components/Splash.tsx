8bad31cbf43d15688a49060c91aa79638921de70
drpurna <94000424+drpurna@users.noreply.github.com>
2026-03-18 15:54:04 +0530

    Create Splash.tsx

---
+++ src/components/Splash.tsx
@@ -0,0 +1,16 @@
+export default function Splash() {
+  return (
+    <div style={{
+      width: "100vw",
+      height: "100vh",
+      background: "black",
+      color: "white",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      fontSize: "48px"
+    }}>
+      📺 Tizen TV
+    </div>
+  );
+}
