38a860407833b715459bfb84eee08d023600f196
drpurna <94000424+drpurna@users.noreply.github.com>
2026-03-28 11:53:29 +0530

    Create index.js

---
+++ app/xtream/index.js
@@ -0,0 +1,9 @@
+// xtream/index.js
+// Main export for Xtream Codes integration
+
+import XtreamClient from './client.js';
+import XtreamCache from './cache.js';
+import XtreamAuth from './auth.js';
+
+export { XtreamClient, XtreamCache, XtreamAuth };
+export default XtreamClient;
